61ecfdc1c7153fee6259bcd4a6ee1235c1805da3
Goody <civilgoody@gmail.com>
2025-06-22 19:22:13 +0100

    refactor(handlers): enhance course creation and retrieval logic

--- internal/handlers/qb.go
+++ internal/handlers/qb.go
@@ -4,6 +4,7 @@ import (
 	"qb/pkg/database"
 	"qb/pkg/models"
 	"qb/pkg/utils"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -67,35 +68,90 @@ func CreateDepartment(c *gin.Context) {
 func GetDepartments(c *gin.Context) {
 	var departments []models.Department
 	
-	if utils.HandleGetResources(c, database.DB, &departments) {
+	if err := database.DB.Preload("Faculty").Find(&departments).Error; err != nil {
+		utils.HandleError(c, utils.ErrDatabase)
 		return
 	}
+	utils.SuccessResponse(c, departments)
 }
 
 func DeleteDepartment(c *gin.Context) {
-	if utils.DeleteResourceByID(c, database.DB, "id", "Department", &models.Department{}) {
+	if utils.DeleteResourceByStringID(c, database.DB, "id", "Department", &models.Department{}) {
 		return
 	}
 }
 
 func CreateCourse(c *gin.Context) {
-	var course models.Course
+	var input models.CreateCourseDTO
 
-	if utils.HandleCreateResource(c, database.DB, utils.Validator, &course) {
+	// Step 1: Bind and validate the DTO
+	if err := c.ShouldBindJSON(&input); err != nil {
+		utils.HandleValidationError(c, err)
 		return
 	}
+
+	if err := utils.Validator.Struct(&input); err != nil {
+		utils.HandleValidationError(c, err)
+		return
+	}
+
+	// Step 2: Parse the course code
+	departmentCode, level, semester, _, err := input.ParseCourseCode()
+	if err != nil {
+		utils.HandleError(c, utils.NewValidationError(err.Error()))
+		return
+	}
+
+	// Step 3: Validate that the department exists
+	var department models.Department
+	if err := database.DB.Where("_id = ?", departmentCode).First(&department).Error; err != nil {
+		utils.HandleError(c, utils.NewValidationError("Department with code '"+departmentCode+"' does not exist"))
+		return
+	}
+
+	// Step 4: Validate that the level exists
+	var levelModel models.Level
+	if err := database.DB.Where("_id = ?", level).First(&levelModel).Error; err != nil {
+		utils.HandleError(c, utils.NewValidationError("Level "+strconv.Itoa(level)+" does not exist"))
+		return
+	}
+
+	// Step 5: Create the course with parsed values (using course code as ID)
+	course := models.Course{
+		ID:          input.Code,
+		Units:       input.Units,
+		Title:       input.Title,
+		LevelID:     level,
+		Semester:    semester,
+		Description: input.Description,
+		Status:      input.Status,
+		Departments: []models.Department{department}, // Associate with the parsed department
+	}
+
+	// Step 6: Create the course in the database
+	if err := database.DB.Create(&course).Error; err != nil {
+		utils.HandleDatabaseError(c, err)
+		return
+	}
+
+	// Step 7: Return success response
+	utils.SuccessResponse(c, course)
 }
 
 func GetCourses(c *gin.Context) {
 	var courses []models.Course
 	
-	if utils.HandleGetResources(c, database.DB, &courses) {
+	// Preload related Level and Departments data
+	if err := database.DB.Preload("Level").Preload("Departments").Find(&courses).Error; err != nil {
+		utils.HandleError(c, utils.ErrDatabase)
 		return
 	}
+	
+	utils.SuccessResponse(c, courses)
 }
 
 func DeleteCourse(c *gin.Context) {
-	if utils.DeleteResourceByID(c, database.DB, "id", "Course", &models.Course{}) {
+	if utils.DeleteResourceByStringID(c, database.DB, "id", "Course", &models.Course{}) {
 		return
 	}
 }
